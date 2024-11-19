cd343122a643001d05284212535981f519ea0bb0
ImAdityaa12 <adityagupta1291@gmail.com>
2024-11-19 15:29:38 +0530

    Added locale and bulkmeta in Readme

--- blocks/customBlock/customBlock.js
+++ blocks/customBlock/customBlock.js
@@ -1,8 +1,15 @@
+import { fetchPlaceholders, getMetadata } from "../../scripts/aem.js";
+
+async function placeholders() {
+  const locale = getMetadata("locale");
+  const placeholders = await fetchPlaceholders(locale);
+  console.log(placeholders);
+}
 export default function decorate(block) {
+  placeholders();
   // setup image columns
   const cardContainerPrev = document.querySelector(".customblock.card.block");
   cardContainerPrev.classList.add("card-container-wrapper");
-
   [...block.children].forEach((row, index) => {
     row.classList.add("card-container");
     const newEl = document.createElement("div");
